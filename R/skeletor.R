9f2e37eec724eb3925af7ef2963056443fad4d0c
Neal Richardson <neal@crunch.io>
2016-04-11 12:39:37 -0400

    A little more cleanup

--- R/skeletor.R
+++ R/skeletor.R
@@ -7,25 +7,30 @@
 skeletor <- function (name, dir=name) {
     ## Make the package dir
     dir.create(dir)
+
     ## Copy the pkg skeleton
     file.copy(file.path(system.file(package="skeletor"), "pkg", "."), dir,
         recursive=TRUE)
+
+    ## cd so that we don't have to carry file.path(dir, ...) everywhere
+    cwd <- setwd(dir)
+    on.exit(setwd(cwd))
+
     ## Rename some things that R CMD CHECK won't allow in inst otherwise
-    file.rename(file.path(dir, "elifekaM"), file.path(dir, "Makefile"))
-    file.rename(file.path(dir, "Rbuildignore"), file.path(dir, ".Rbuildignore"))
-    file.rename(file.path(dir, "Rinstignore"), file.path(dir, ".Rinstignore"))
-    file.rename(file.path(dir, "travis.yml"), file.path(dir, ".travis.yml"))
-    file.rename(file.path(dir, "gitignore"), file.path(dir, ".gitignore"))
+    file.rename("elifekaM", "Makefile")
+    file.rename("Rbuildignore", ".Rbuildignore")
+    file.rename("Rinstignore", ".Rinstignore")
+    file.rename("travis.yml", ".travis.yml")
+    file.rename("gitignore", ".gitignore")
 
     ## Create empty dirs
-    dir.create(file.path(dir, "man"))
-    dir.create(file.path(dir, "vignettes"))
+    dir.create("man")
+    dir.create("vignettes")
 
     ## Sub in the package name appropriately
     files.to.edit <- c("DESCRIPTION", "Makefile", "NEWS.md", "README.md",
         ".gitignore", file.path("tests", "testthat.R"))
     lapply(files.to.edit, function (f) {
-        f <- file.path(dir, f)
         cleaned <- gsub("yourpackagename", name, readLines(f))
         writeLines(cleaned, f)
     })
