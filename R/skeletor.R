25f64b88d66937456377007d40a3f4251f141049
Neal Richardson <neal@crunch.io>
2016-04-11 12:24:56 -0400

    Passing check now

--- R/skeletor.R
+++ R/skeletor.R
@@ -10,6 +10,17 @@ skeletor <- function (name, dir=name) {
     ## Copy the pkg skeleton
     file.copy(file.path(system.file(package="skeletor"), "pkg", "."), dir,
         recursive=TRUE)
+    ## Rename some things that R CMD CHECK won't allow in inst otherwise
+    file.rename(file.path(dir, "elifekaM"), file.path(dir, "Makefile"))
+    file.rename(file.path(dir, "Rbuildignore"), file.path(dir, ".Rbuildignore"))
+    file.rename(file.path(dir, "Rinstignore"), file.path(dir, ".Rinstignore"))
+    file.rename(file.path(dir, "travis.yml"), file.path(dir, ".travis.yml"))
+    file.rename(file.path(dir, "gitignore"), file.path(dir, ".gitignore"))
+
+    ## Create empty dirs
+    dir.create(file.path(dir, "man"))
+    dir.create(file.path(dir, "vignettes"))
+
     ## Sub in the package name appropriately
     files.to.edit <- c("DESCRIPTION", "Makefile", "NEWS.md", "README.md",
         file.path("tests", "testthat.R"))
--- tests/testthat/test-skeletor.R
+++ tests/testthat/test-skeletor.R
@@ -1,11 +1,23 @@
 context("Skeletor")
 
 public({
+    dest <- file.path(tempdir(), "testskeletor")
     test_that("Creating a package skeleton", {
-        dest <- file.path(tempdir(), "testskeletor")
         skeletor("testskeletor", dest)
         expect_true(dir.exists(dest))
+    })
+    test_that("The right dirs exist", {
         expect_true(dir.exists(file.path(dest, "tests")))
+        expect_true(dir.exists(file.path(dest, "man")))
+        expect_true(dir.exists(file.path(dest, "vignettes")))
+    })
+    test_that("The right files exist", {
+        expect_true(file.exists(file.path(dest, "DESCRIPTION")))
+        expect_true(file.exists(file.path(dest, ".Rbuildignore")))
+        expect_true(file.exists(file.path(dest, "Makefile")))
+        expect_true(file.exists(file.path(dest, ".gitignore")))
+    })
+    test_that("The package name appears in the contents", {
         desc <- readLines(file.path(dest, "DESCRIPTION"))
         expect_identical(desc[1], "Package: testskeletor")
         tests <- readLines(file.path(dest, "tests", "testthat.R"))
