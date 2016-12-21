e39271b48146ac5c41b3eb402586beab9c215533
Neal Richardson <neal@crunch.io>
2016-12-21 12:53:51 -0800

    Add configure function to save name, email, github to .Rprofile

---
+++ R/configure.R
@@ -0,0 +1,61 @@
+#' Set some personal defaults for skeletor
+#' @param name character: your name
+#' @param email character: your email address
+#' @param github character: your GitHub account
+#' @param file character: file path to set these in as options. Default is your
+#' personal .Rprofile file.
+#' @return Invisibly, the options set in \code{file}. Typically, you'll call
+#' this function only once, and only for its side effects of adding the values
+#' to your .Rprofile so that \code{\link{skeletor}} will find them there without
+#' your having to enter them each time.
+#' @export
+configure <- function (name=NULL, email=NULL, github=NULL, file="~/.Rprofile") {
+    ## Construct string to write and payload to return
+    out <- list()
+    opts <- c()
+    if (!is.null(name)) {
+        out$skeletor.name <- name
+        opts <- c(opts, paste0('skeletor.name="', name, '"'))
+    }
+    if (!is.null(email)) {
+        out$skeletor.email <- email
+        opts <- c(opts, paste0('skeletor.email="', email, '"'))
+    }
+    if (!is.null(github)) {
+        out$skeletor.github <- github
+        opts <- c(opts, paste0('skeletor.github="', github, '"'))
+    }
+    if (length(opts)) {
+        opts <- paste0("options(", paste(opts, collapse=", "),
+            ") # Added by skeletor::configure")
+    } else {
+        ## If all are null, we aren't going to write anything
+        opts <- ""
+    }
+
+    ## If file does not exist, and we have options to add, just cat to it
+    if (!file.exists(file)) {
+        if (nchar(opts)) {
+            cat(paste0(opts, "\n"), file=file)
+        }
+    } else {
+        ## Read in the file to look for our comment
+        lines <- readLines(file)
+        ours <- grep("^options\\(.*\\) # Added by skeletor", lines)
+        if (length(ours)) {
+            ## Drop those
+            lines <- lines[-ours]
+        }
+        if (nchar(opts)) { ## Only add ours if it is non-empty
+            ## Add a line of padding if needed
+            if (nchar(lines[length(lines)])) {
+                lines <- c(lines, "")
+            }
+            ## Add our line
+            lines <- c(lines, opts)
+        }
+        ## Write out
+        writeLines(lines, file)
+    }
+    invisible(out)
+}
---
+++ tests/testthat/test-configure.R
@@ -0,0 +1,48 @@
+context("Configuration")
+
+public({
+    f <- tempfile()
+    test_that("configure adds a line to your .Rprofile (or other specified file)", {
+        expect_false(file.exists(f))
+        cat("abc\n123\n", file=f) ## Now the file exists
+        configure(name="State Your Name", email="sy.name@example.com",
+            github="syn", file=f) ## Provide the file; default arg is ~/.Rprofile
+        out <- readLines(f)
+        expect_identical(out, c("abc", "123", "",
+            'options(skeletor.name="State Your Name", skeletor.email="sy.name@example.com", skeletor.github="syn") # Added by skeletor::configure'))
+    })
+    test_that("configure overwrites its own line if you run it again", {
+        configure(name="Your Name Here", email="ynh@example.com", file=f)
+        expect_identical(readLines(f), c("abc", "123", "",
+            'options(skeletor.name="Your Name Here", skeletor.email="ynh@example.com") # Added by skeletor::configure'))
+    })
+
+    test_that("configure removes its line if you run it with empty args", {
+        cfg <- configure(file=f)
+        expect_identical(readLines(f), c("abc", "123", ""))
+        expect_identical(cfg, list())
+    })
+
+    f2 <- tempfile()
+    cfg <- configure(name="State Your Name", email="sy.name@example.com",
+        github="syn", file=f2)
+    test_that("configure creates the file if it does not already exist", {
+        out <- readLines(f2)
+        expect_identical(out,
+            'options(skeletor.name="State Your Name", skeletor.email="sy.name@example.com", skeletor.github="syn") # Added by skeletor::configure')
+    })
+
+    test_that("configure returns its set options invisibly", {
+        expect_identical(cfg,
+            list(skeletor.name="State Your Name",
+                 skeletor.email="sy.name@example.com",
+                 skeletor.github="syn"))
+    })
+
+    test_that("configure with empty args doesn't create a file if it doesn't exist", {
+        f3 <- tempfile()
+        expect_false(file.exists(f3))
+        configure(file=f3)
+        expect_false(file.exists(f3))
+    })
+})
